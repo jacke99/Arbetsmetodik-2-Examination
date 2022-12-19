5d5615366b30f460c1d3dfc4e4cf13a73ee6f541
jacob von scheele <jacobvonscheele@outlook.com>
2022-12-19 09:35:23 +0100

    Added balance funtion

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const mainWrapper = document.querySelector(".main-wrapper");
+const searchBar = document.querySelector(".search-bar");
 
 const flag = document.querySelector(".flag");
+let english = true;
 
 const filterBtnAll = document.querySelector(".filter-btn-all");
 const filterBtnFood = document.querySelector(".filter-btn-food");
 const filterBtnDrinks = document.querySelector(".filter-btn-drinks");
+const filterBtnDessert = document.querySelector(".filter-btn-dessert");
 
 const allFoods = db["our-foods"];
 const allDrinks = db.drinks;
@@ -14,6 +17,9 @@ const shoppingCartDot = document.querySelector(".shopping-cart-dot");
 const cartBtn = document.querySelector(".cart-btn");
 
 const orders = document.querySelector(".orders");
+const ordersBalance = document.querySelector(".orders-balance");
+const addBalanceInput = document.querySelector(".add-balance");
+let balance = 0;
 const closeBtn = document.querySelector(".close-order");
 
 let drinksToDisplay = [];
@@ -28,25 +34,40 @@ categorys.splice(categorys.indexOf("best-foods"), 1);
 categorys.splice(categorys.indexOf("our-foods"), 1);
 categorys.splice(categorys.indexOf("pagination"), 1);
 
-/* Loopar igenom kategorierna och tar en från varje in till listan, kan ökas till flera */
-for (let i = 0; i < categorys.length; i++) {
-  for (let x = 0; x < 1; x++) {
-    foodToDisplay.push(db[categorys[i]][x]);
+function removeAllCards() {
+  /* Tar bort alla befintliga kort och resetta array*/
+  drinksToDisplay = [];
+  foodToDisplay = [];
+  let allExistingCards = document.querySelectorAll(".card-container");
+  allExistingCards.forEach((element) => {
+    element.remove();
+  });
+}
+
+function addAllFoods() {
+  removeAllCards();
+  /* Loopar igenom kategorierna och tar en från varje in till listan, kan ökas till flera */
+  for (let i = 0; i < categorys.length; i++) {
+    for (let x = 0; x < 1; x++) {
+      foodToDisplay.push(db[categorys[i]][x]);
+    }
   }
+
+  // Tar ett antal drycker och stoppar in dom i listan
+  for (let i = 0; i < amountOfDrinks; i++) {
+    drinksToDisplay.push(db["drinks"][i]);
+  }
+
+  foodToDisplay.forEach((element) => {
+    createCard(element.name, element.img, element.price, element.dsc);
+  });
+
+  drinksToDisplay.forEach((element) => {
+    createCard(element.name, element.img, element.price, element.dsc);
+  });
 }
 
-// Tar ett antal drycker och stoppar in dom i listan
-for (let i = 0; i < amountOfDrinks; i++) {
-  drinksToDisplay.push(db["drinks"][i]);
-}
-
-foodToDisplay.forEach((element) => {
-  createCard(element.name, element.img, element.price, element.dsc);
-});
-
-drinksToDisplay.forEach((element) => {
-  createCard(element.name, element.img, element.price, element.dsc);
-});
+addAllFoods();
 
 function createCard(name, img, price, desc) {
   mainWrapper.innerHTML += `
@@ -113,14 +134,46 @@ closeBtn.addEventListener("click", () => {
   cartBtn.style.display = "block";
 });
 
+addBalanceInput.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    if (balance == 0) {
+      balance = parseInt(addBalanceInput.value);
+    } else {
+      balance += parseInt(addBalanceInput.value);
+    }
+    ordersBalance.textContent = `Saldo: ${balance}`;
+    addBalanceInput.value = "";
+  }
+});
+
+/*
+  
+  
+ 
+ */
+
 flag.addEventListener("click", () => {
   if (english == true) {
     // Ändra allt till svenska
+    filterBtnAll.innerText = "Allt";
+    filterBtnFood.innerText = "Mat";
+    filterBtnDrinks.innerText = "Dryck";
+    filterBtnDessert.innerText = "Efterrätt";
 
+    searchBar.placeholder = "Sök...";
+
+    closeBtn.innerText = "Stäng";
     english = false;
   } else {
     //Ändra allt till engelska
+    filterBtnAll.innerText = "All";
+    filterBtnFood.innerText = "Food";
+    filterBtnDrinks.innerText = "Drinks";
+    filterBtnDessert.innerText = "Dessert";
 
+    searchBar.placeholder = "Search...";
+
+    closeBtn.innerText = "Close";
     english = true;
   }
 });
